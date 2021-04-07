a68f548cc2375e9808adecc13b1b756a52d18b74
Sundar Acharya <Sundaracharya096@gmail.com>
2021-04-07 14:36:46 +0545

    Social link working update

--- scripts/default.js
+++ scripts/default.js
@@ -7,10 +7,27 @@ $(document).ready(function () {
         $(this).addClass('active').siblings().removeClass('active');
         var child = $(this).attr('id') + "-contents >div";
         console.log(preNavAbout);
-        $('#'+preNavAbout+' >div').removeClass('active');
-        preNavAbout = $(this).attr('id')+'-contents';
+        $('#' + preNavAbout + ' >div').removeClass('active');
+        preNavAbout = $(this).attr('id') + '-contents';
         $('#' + child).addClass('active').siblings().addClass('active');
         console.log(child);
         $('#' + activeId).addClass('active').siblings().removeClass('active');
     });
+    $('#btnSubmit').click(function () {
+        submitButton();
+    });
 });
+function submitButton() {
+    var mName = $("#mName")[0].value;
+    var mSubject = $("#mSubject")[0].value;
+    if (mSubject == "") {
+        mSubject = "No Subject";
+    }
+    var mEmail = $("#mEmail")[0].value;
+    var mMessage = $("#message")[0].value;
+    var body = mName + "    " + mEmail + "     " + mMessage;
+    if(body==""){
+        body = 'Message Here';
+    }
+    $('#contact-form a').attr('href', 'mailto:sundaracharya096@gmail.com?' + 'subject=' + mSubject + '&body=' + body);
+};
